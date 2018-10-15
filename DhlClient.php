584f5e2a2acfc5339502c1159032ab522d99010b
Steven Sack <info@stevensack.nl>
2018-10-15 16:24:55 +0200

    Added functionality for adding pickup requests

--- DhlClient.php
+++ DhlClient.php
@@ -5,6 +5,7 @@ namespace Keesschepers\DhlParcelApi;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Psr7\Response;
 use kamermans\OAuth2\GrantType\ClientCredentials;
 use kamermans\OAuth2\OAuth2Middleware;
 use Keesschepers\DhlParcelApi\DhlApiException;
@@ -62,6 +63,31 @@ class DhlClient
 
         if (200 !== $response->getStatusCode()) {
             throw new DhlApiException('Could not retrieve time window information due to API server error.');
+        }
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * @param array $parameters
+     * @return mixed
+     * @throws \Keesschepers\DhlParcelApi\DhlApiException
+     */
+    public function createPickupRequest(array $parameters)
+    {
+        $this->setupClient();
+
+        try {
+            /** @var Response $response */
+            $response = $this->httpClient->post(
+                '/pickup-requests',
+                [
+                    'timeout' => ($this->apiTimeout / 1000),
+                    'json' => $parameters,
+                ]
+            );
+        } catch (BadResponseException $e) {
+            throw new DhlApiException(sprintf('Could not could not create a label due to API server error: %s', $e->getResponse()->getBody()));
         }
 
         return json_decode($response->getBody()->getContents(), true);
